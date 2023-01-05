6f76c9af591fd6a9a14405890f61b16f9db18591
youssef-qteishat <youssef.qt1@gmail.com>
2023-01-04 19:32:58 -0800

    complete level 3 authentication w/ md5 hash function

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const {Schema} = mongoose;
 
@@ -26,7 +26,7 @@ const userSchema = new Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = mongoose.model('User', userSchema);
 
@@ -45,7 +45,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.username
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err){
@@ -58,7 +58,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password)
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
